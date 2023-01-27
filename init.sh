f87a306b4e7cc5553750d9f50a9768d9ebd722a0
Ivan <ivankvl7@gmail.com>
2023-01-27 14:37:14 +0600

    2.1.11 Django + WSGI

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & curl 'http://127.0.0.1:8080/?a=1&a=2&b=3'
