712066e6b7e363b16b2b7726de71ffc087b55488
Ivan <ivankvl7@gmail.com>
2023-01-27 17:33:29 +0600

    1

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g)_w27husr1c0(78*q*e-_=bm49g1hoo3wu@da)6d@7lhwoda=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ask',
     'qa',
 ]
 
--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
+sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
 
